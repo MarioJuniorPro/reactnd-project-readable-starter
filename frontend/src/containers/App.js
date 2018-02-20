56fb1e47264bdb0ccce242e67faedeec6cde0e51
Mario Costa Junior <mario.junior.pro@gmail.com>
2018-02-20 13:16:43 -0300

    refactor: change app style

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <header className="page__header">Header</header>
+        <header className="page__header">Project Readable</header>
         <main className="page__main">
           <nav className="posts__categories">
             <ul className="categories-list">
@@ -23,23 +23,24 @@ class App extends Component {
             </ul>
           </nav>
           <section className="posts-cards">
+            { [1,2,3,4,5,6].map(e => (
             <article className="posts-card">
               <div className="vote-score">
                 <button className="vote-score__increase" title="Increment">&uarr;</button>
                 <span className="vote-score__counter">0</span>
                 <button className="vote-score__increase" title="Decrement">&darr;	</button>
               </div>
-              <div className="posts-card__info-container">
-                <h2 className="posts-card__title">
-                  <a href="#">Learn Redux in 10 minutes!</a>
-                </h2>
-                <span className="posts-card__author">Necrower</span>
-                <p className="posts-card__comments-count">7 comments</p>              
+
+                <div className="posts-card__info-container">
+                <span className="posts-card__title">
+                  <a href="#" className="posts-card__link">Learn Redux in 10 minutes!</a>
+                </span>
+                <span className="posts-card__author">Author: Necrower</span>
+                <span className="posts-card__short-description">Just kidding. It takes more than 10 minutes to learn technology.</span>
+                <span className="posts-card__comments-count">7 comments</span>              
               </div>
             </article>
-            <article className="posts-card">Post 2</article>
-            <article className="posts-card">Post 3</article>
-            <article className="posts-card">Post 4</article>
+            ))}
           </section>
         </main>
         <footer className="page__footer">Footer</footer>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/reset.css';
 import './css/index.css';
-import './css/theme.css';
 import Root from './Root';
 // import registerServiceWorker from './registerServiceWorker';
 
